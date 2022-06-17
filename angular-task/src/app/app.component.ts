6a6b89aa1901837741dc11fd265c7dcc0731c1bd
OlehKachurak <olegxpress7@gmail.com>
2022-06-17 23:05:31 +0300

    figma task

--- angular-task/src/app/app.component.ts
+++ angular-task/src/app/app.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, Injectable, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map, Observable } from 'rxjs';
+import { KeyValuePipe } from '@angular/common';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [KeyValuePipe]
 })
-export class AppComponent {
-  title = 'angular-task';
-}
+@Injectable()
+export class AppComponent implements OnInit {
+  popup = false
+  name = 'Angular';
+  private _jsonURL = 'assets/loans.json';
+  constructor(private http: HttpClient,private keyValue: KeyValuePipe) {
+   }
+  public getJSON(): Observable<any> {
+    return this.http.get(this._jsonURL);
+  }
+
+  loans:any[] = [];
+
+  ngOnInit():void {
+    this.getJSON().subscribe(data => {
+      data.loans;
+      this.loans = data.loans;
+     console.log(data.loans);
+    });
+    
+   
+  }
+  
+  
+ }
--- angular-task/src/app/app.module.ts
+++ angular-task/src/app/app.module.ts
@@ -1,16 +1,27 @@
-import { NgModule } from '@angular/core';
+import { NgModule ,NO_ERRORS_SCHEMA,CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
+import { MatDialogModule } from '@angular/material/dialog';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatFormFieldModule} from '@angular/material/form-field';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    MatDialogModule,
+    BrowserAnimationsModule,
+    MatFormFieldModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas: [
+    CUSTOM_ELEMENTS_SCHEMA,
+    NO_ERRORS_SCHEMA
+  ]
 })
 export class AppModule { }
